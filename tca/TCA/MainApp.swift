82abe5a3226710ab0e1edb8560718d21a0c2f1ff
una <unagamix@gmail.com>
2023-02-02 22:49:56 +0900

    add: tca initial

---
+++ tca/TCA/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  TCA
+//
+//  Created by unagami on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
