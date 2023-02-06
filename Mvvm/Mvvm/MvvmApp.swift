212510d5adb139c6e5ecf69231acd81264b0d834
una <unagamix@gmail.com>
2023-02-06 22:26:58 +0900

    feat: create project

---
+++ Mvvm/Mvvm/MvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmApp.swift
+//  Mvvm
+//
+//  Created by unagami on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct MvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
